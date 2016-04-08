af4c1355c36c4d91f6777e68ac332b9ceccd48b1
RatchyZanko <ratchapong.t@gmail.com>
2016-04-08 01:33:44 -0400

    Use external libs

--- src/Parser.java
+++ src/Parser.java
@@ -2,60 +2,57 @@
  * Created by RatchyZanko on 4/8/16.
  */
 
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+
 import java.io.*;
 
 public class Parser {
     public static void main(String[] args) {
-        String fileName = "problems/problems.txt";
-        String outputName = "output/leetcode-problems.json";
-        String line;
-        String jsonObject = "";
+
+        Document doc = null;
         try {
-            FileReader fileReader = new FileReader(fileName);
-
-            BufferedReader bufferedReader = new BufferedReader(fileReader);
-
-            while ((line = bufferedReader.readLine()) != null) {
-                int i = 0;
-                String numberString = "";
-                while (line.charAt(i) != ' ') {
-                    numberString = numberString + line.charAt(i);
-                    i++;
-                }
-                while (line.charAt(i) == ' ') {
-                    i++;
-                }
-                int number = Integer.parseInt(numberString);
-                String title = line.substring(i, line.indexOf('%') - 4).trim();
-                String difficulty = line.substring(line.indexOf('%') + 1, line.length()).trim();
-                jsonObject = jsonObject +
-                        "{ " +
-                        "\"number\":" + "\"" + number + "\"" + "," +
-                        "\"title\":" + "\"" + title + "\"" + "," +
-                        "\"difficulty\":" + "\"" + difficulty + "\""
-                        + "},";
-
-            }
-            jsonObject = jsonObject.substring(0, jsonObject.length() - 1);
-            jsonObject = "{" + "\"leetcode-problems\": [" + jsonObject + "]}";
-            bufferedReader.close();
-        } catch (FileNotFoundException ex) {
-            System.out.println(
-                    "Unable to open file '" +
-                            fileName + "'");
-        } catch (IOException ex) {
-            System.out.println(
-                    "Error reading file '"
-                            + fileName + "'");
+            doc = Jsoup.connect("https://leetcode.com/problemset/algorithms/").get();
+        } catch (IOException e) {
+            e.printStackTrace();
         }
+        Elements problemsTable = doc.select("#problemList");
+        Elements tableBody = problemsTable.select("tbody");
+        JSONObject leetcodeProblem = new JSONObject();
+        JSONArray jsonArray = new JSONArray();
+        for (Element row : tableBody.select("tr")) {
+            Elements data = row.select("td");
+            int number = Integer.parseInt(data.get(1).text());
+            String title = data.get(2).text();
+            double rate = Double.parseDouble(data.get(3).text().substring(0, data.get(3).text().length() - 1));
+            String link = data.get(2).select("a").first().attr("abs:href");
+            boolean lock = data.get(2).select("i").first() != null;
+            String difficulty = data.get(5).text();
+
+            JSONObject obj = new JSONObject();
+            obj.put("number", number);
+            obj.put("title", title);
+            obj.put("rate", rate);
+            System.out.println(link);
+            obj.put("link", link);
+            obj.put("lock", lock);
+            obj.put("difficulty", difficulty);
+            jsonArray.add(obj);
+
+
+        }
+        leetcodeProblem.put("leetcode-problems", jsonArray);
+        String outputName = "output/leetcode-problems.json";
 
         try {
-            FileWriter fileWriter = new FileWriter(outputName);
-            BufferedWriter bufferedWriter =
-                    new BufferedWriter(fileWriter);
-            
-            bufferedWriter.write(jsonObject);
-            bufferedWriter.close();
+            FileWriter file = new FileWriter(outputName);
+            file.write(leetcodeProblem.toJSONString());
+            file.flush();
+            file.close();
         } catch (IOException e) {
             e.printStackTrace();
         }
